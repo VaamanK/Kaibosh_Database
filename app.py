2707c5279e7ab6364aa3d1eb96972abaf6274bcb
Vaaman Khandelwal <22451@wc.school.nz>
2025-04-07 15:13:41 +1200

    Made URLs on the website. Need to figure out why the website prints empty files when I have headings in there.

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def homepage():  # put application's code here
-    return 'Hello World!'
+def homepage():
+    return render_template('homepage.html')
 
+@app.route('/input')
+def input():
+    return render_template('input.html')
+
+@app.route('/sort')
+def sort():
+    return render_template('sort.html')
+
+@app.route('/output')
+def output():
+    return render_template('output.html')
 
 if __name__ == '__main__':
     app.run()
