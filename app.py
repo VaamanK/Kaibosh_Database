4c42be1900a8c428fe22c47a5b2ae24867cdffd5
VaamanK <22451@wc.school.nz>
2025-05-26 14:19:24 +1200

    Made Login Page. Currently only visuals. Next I need to work on making it actually functional.

--- app.py
+++ app.py
@@ -20,5 +20,14 @@ def sort():
 def output():
     return render_template('output.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
 if __name__ == '__main__':
     app.run()
