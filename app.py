8627418e1bdd7fff961bb34ad3b67971f78542af
VaamanK <22451@wc.school.nz>
2025-06-01 15:26:44 +1200

    Made signup and login page. Currently front end, need to work on back end.

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template
+import sqlite3
+from sqlite3 import Error
+
+DATABASE = "kaibosh_table"
 
 app = Flask(__name__)
 
-
+def connect_database(db_file):
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except Error as e:
+        print(e)
+        print(f'Error while connecting to database: {e}')
+    return None
 
 @app.route('/')
 def homepage():
@@ -20,12 +31,15 @@ def sort():
 def output():
     return render_template('output.html')
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
+
     return render_template('login.html')
 
-@app.route('/signup')
+@app.route('/signup', methods=['POST', 'GET'])
 def signup():
+    con = connect_database(DATABASE)
+    query = "SELECT "
     return render_template('signup.html')
 
 
